400c581bc46fbe3b0ec33a7a68e180ee7293cf43
Việt Blogger <50909768+vietbloggerdesign@users.noreply.github.com>
2019-07-25 18:36:21 +0700

    Update urlencode_unpacker.js

--- urlencode_unpacker.js
+++ urlencode_unpacker.js
@@ -2,18 +2,14 @@ var isNode = (typeof module !== 'undefined' && module.exports);
 if (isNode) {
   var SanityTest = require(__dirname + '/../../test/sanitytest');
 }
-
 var Urlencoded = {
   detect: function(str) {
-    // the fact that script doesn't contain any space, but has %20 instead
-    // should be sufficient check for now.
     if (str.indexOf(' ') === -1) {
       if (str.indexOf('%2') !== -1) return true;
       if (str.replace(/[^%]+/g, '').length > 3) return true;
     }
     return false;
   },
-
   unpack: function(str) {
     if (Urlencoded.detect(str)) {
       if (str.indexOf('%2B') !== -1 || str.indexOf('%2b') !== -1) {
@@ -25,9 +21,6 @@ var Urlencoded = {
     }
     return str;
   },
-
-
-
   run_tests: function(sanity_test) {
     var t = sanity_test || new SanityTest();
     t.test_function(Urlencoded.detect, "Urlencoded.detect");
@@ -38,7 +31,6 @@ var Urlencoded = {
     t.expect('var%20%21%22', true);
     t.expect('javascript:(function(){var%20whatever={init:function(){alert(%22a%22+%22b%22)}};whatever.init()})();', true);
     t.test_function(Urlencoded.unpack, 'Urlencoded.unpack');
-
     t.expect('javascript:(function(){var%20whatever={init:function(){alert(%22a%22+%22b%22)}};whatever.init()})();',
       'javascript:(function(){var whatever={init:function(){alert("a"+"b")}};whatever.init()})();'
     );
@@ -50,10 +42,7 @@ var Urlencoded = {
     t.expect('var%20a=b%2b1', 'var a=b+1');
     return t;
   }
-
-
 };
-
 if (isNode) {
   module.exports = Urlencoded;
 }
