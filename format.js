83d02846951c9d618da6d99c0adf199fd5e6d067
Việt Blogger <50909768+vietbloggerdesign@users.noreply.github.com>
2019-07-25 18:25:47 +0700

    Update format.js

--- format.js
+++ format.js
@@ -1,19 +1,19 @@
-self.addEventListener('message', function (e) {
-    var source = e.data.source;
-    if (e.data.beautify) {
-        self._window = self.window;
-        self.window = {};
-        self.importScripts('https://vietbloggerdesign.github.io/de4js/beautify.min.js');
-        source = self.window.js_beautify(source, {
-            unescape_strings: true,
-            jslint_happy: true
-        });
-        self.window = self._window;
-    }
-    self.importScripts('https://vietbloggerdesign.github.io/de4js/highlight.min.js');
-    source = self.hljs.highlight('javascript', source).value;
-    source = source.split('\n');
-    source = source.join('</code><code>');
-    source = '<code>' + source + '</code>';
-    self.postMessage(source);
+self.addEventListener('message', function(e) {
+  var source = e.data.source;
+  if (e.data.beautify) {
+    self._window = self.window;
+    self.window = {};
+    self.importScripts('https://vietbloggerdesign.github.io/de4js/beautify.min.js');
+    source = self.window.js_beautify(source, {
+      unescape_strings: true,
+      jslint_happy: true
+    });
+    self.window = self._window;
+  }
+  self.importScripts('https://vietbloggerdesign.github.io/de4js/highlight.min.js');
+  source = self.hljs.highlight('javascript', source).value;
+  source = source.split('\n');
+  source = source.join('</code><code>');
+  source = '<code>' + source + '</code>';
+  self.postMessage(source);
 });
