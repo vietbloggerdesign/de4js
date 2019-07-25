8415377f820891be50596268c7d30d80e421beaa
Việt Blogger <50909768+vietbloggerdesign@users.noreply.github.com>
2019-07-25 13:57:13 +0700

    Create format.js

---
+++ format.js
@@ -0,0 +1,26 @@
+self.addEventListener('message', function (e) {
+    var source = e.data.source;
+
+    if (e.data.beautify) {
+        self._window = self.window;
+        self.window = {};
+
+        self.importScripts('/de4js/assets/js/lib/js-beautify/beautify.min.js');
+
+        source = self.window.js_beautify(source, {
+            unescape_strings: true,
+            jslint_happy: true
+        });
+
+        self.window = self._window;
+    }
+
+    self.importScripts('/de4js/assets/js/lib/highlight-js/highlight.min.js');
+
+    source = self.hljs.highlight('javascript', source).value;
+    source = source.split('\n');
+    source = source.join('</code><code>');
+    source = '<code>' + source + '</code>';
+
+    self.postMessage(source);
+});
